b9a3566a0ee04b7de8a2f9722acafd8d8b300bf2
Srikant Patnaik <usp71084@gmail.com>
2012-04-10 22:19:42 +0530

    Still testing code.
    Added separate files for different puposes. Every command line
    arguement starts with an integer number which signifies the purpose
    of the command. The command is double checked by vcutter.py.
    At present only first option is working.
    BUG: An o/p file is created before the time of execution. User need
    to enter y/N to over ride.

--- vcutter-cli/arguements.py
+++ vcutter-cli/arguements.py
@@ -15,7 +15,17 @@ class argParse:
                                          epilog="Epilog goes here",
                                          prefix_chars = '-',
                                          add_help = True)
-        
+        parser.add_argument('select',
+                            metavar='N',
+                            type = int,
+                            nargs = 1,
+                            help = 'See the help for correct option.')
+
+        parser.add_argument('-i','--input',
+                            metavar = '',
+                            type = argparse.FileType('r'),
+                            help = 'input: file name or path of the input video')
+                             
         parser.add_argument('-a','--add',
                             metavar = '',
                             type = argparse.FileType('r'),
@@ -26,12 +36,7 @@ class argParse:
                             type = argparse.FileType('r'),
                             help = 'cut: cut the video at specified time')
     
-        parser.add_argument('inVideofile',
-                            action = 'store',
-                            nargs='?',
-                            type=argparse.FileType('r'),
-                            help = 'file name or path of the input video')
-    
+
         parser.add_argument('-b','--beginning',
                             metavar = '',
                             type = argparse.FileType('r'),
@@ -50,17 +55,10 @@ class argParse:
                             default = False,
                             help = 'start_time, end_time')
          
-        parser.add_argument('-i','--insert',
+        parser.add_argument('-I','--insert',
                             metavar = '',
                             type = argparse.FileType('r'),
                             help = 'beginning: add in the beginning')
-    
-       
-        parser.add_argument('inVideofile',
-                            action = 'store',
-                            nargs='?',
-                            type=argparse.FileType('r'),
-                            help = 'file name or path of the input video')
     
         parser.add_argument('-o','--out',
                             metavar = '',
@@ -68,16 +66,13 @@ class argParse:
                             help = 'output: output video')
         
         parser.add_argument('--version', action='version', version='%(prog)s 1.0')
-        print '\n'
-        parser.print_help()
-        os.system('cat examples.txt')
-        parser.exit()
+        
+        if len(sys.argv) <= 4:
+            parser.print_help()
+            os.system('cat examples.txt')
+            parser.exit()
+        else:    
+            parser.parse_args()             # This will check the validity of options
+            args = sys.argv[1:]             # If above line returns true then grab the args
+        return args
 
-
-
-if __name__ == "__main__":
-    if len(sys.argv) == 1 :
-        print "vCutter Incorrect usage, please see 'vCutter.py --help'"
-    else :
-        main=argParse()
-        main.parse()
